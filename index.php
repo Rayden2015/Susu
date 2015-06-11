f182d24c33ea431f82e7fe5f807d00af7b32c775
Emmanuel Asaber <emmanuelasaber@gmail.com>
2015-06-11 13:35:55 +0000

    This commit adds the login page

--- index.php
+++ index.php
@@ -1,7 +1,30 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ME
- * Date: 6/9/15
- * Time: 11:11 PM
- */ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Nurudin Lartey, Emmanuel Asaber, Infinixel">
+    <title>Login | Daily Susu</title>
+    <link rel="stylesheet" href="css/semantic.css">
+    <link rel="stylesheet" href="css/entypo/css/entypo.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="img/fav.jpg">
+</head>
+<body>
+    
+    <div class="login">
+        <h2 class="header">Login</h2>
+        <form action="" class="ui form">
+            <div class="field">
+                <label for="">Username</label>
+                <input type="text">
+            </div>
+            <div class="field">
+                <label for="">Password</label>
+                <input type="password">
+            </div>
+            <button type="submit" class="ui button blue">Login</button>
+        </form>
+    </div>
+
+</body>
+</html>
